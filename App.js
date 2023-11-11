3f984e102655c5eab158f3700a07aa7e44c6b914
Andrew Li <54134293+andrlime@users.noreply.github.com>
2023-11-11 17:26:51 -0600

    step1 answers

--- App.js
+++ App.js
@@ -1,11 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentEditIndex, setCurrentEditIndex] = useState(-1);
+
+  const addTask = () => {
+    if(isEditing) {
+      stopEditing();
+      let cur_tasks = tasks;
+      cur_tasks[currentEditIndex] = task;
+      setTasks(cur_tasks);
+      setTask('');
+    } else {
+      if (task.trim() !== '') {
+        setTasks([...tasks, task]);
+        setTask('');
+      }
+    }
+  };
+
+  const clearTasks = () => {
+    setTasks([]);
+    stopEditing();
+  }
+
+  const setEditing = (index) => {
+    setIsEditing(true);
+    setCurrentEditIndex(index)
+  }
+
+  const stopEditing = () => {
+    setIsEditing(false);
+    setCurrentEditIndex(-1);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style={styles.heading}>Simple Todo List</Text>
+
+      <Text style={styles.label}>{isEditing ? `Now editing task ${currentEditIndex + 1}` : "Adding new task"}</Text>
+      <TextInput
+        style={styles.inputBox}
+        value={task}
+        onChangeText={(text) => setTask(text)}
+        label="Add a task"
+        placeholder={"Describe what you need to do here"}
+      />
+
+      <View style={{ display: "flex", flexDirection: "row"}}>
+        <Button title={isEditing ? "Change" : "Add"} onPress={addTask} />
+        <Button color={"red"} title="Clear Tasks" onPress={clearTasks} />
+      </View>
+
+      {tasks.map((task, index) => (
+        <View key={index} style={{ flexDirection: 'row', alignItems: 'center', margin: 5, padding: 10, borderWidth: 1 }}>
+          <Text style={{ flex: 1 }}>{index + 1}</Text>
+          <Text style={{ flex: 1 }}>{task}</Text>
+          <Button title="Edit" onPress={() => {
+            setEditing(index);
+          }} />
+        </View>
+      ))}
+
     </View>
   );
 }
@@ -15,6 +75,22 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    padding: 24,
   },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 16
+  },  
+  inputBox: {
+    margin: 4,
+    borderWidth: 1,
+    borderColor: "#000000",
+    padding: 8,
+    width: "80%"
+  }
 });
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Step 1: Writing your first React Native code
-Now that your development environment is set up, you will write your first code in React Native! In this mini project, we will make a simple todo list. The features will include adding new tasks, deleting tasks, editing tasks, and changing the color for each task.
+Now that your development environment is set up, you will write your first code in React Native! In this mini project, we will make a simple todo list. The features will include adding new tasks, deleting tasks, editing tasks, and making an aesthetically pleasing design. In Step 1, we will focus on the functionality. In Step 2, we will focus on the design.
 
-To help you not spend too much time thinking about this, we have provided some template code. We will guide you through filling in the blanks.
+To help you not spend too much time thinking about this, we have provided some template code. We will guide you through filling in the blanks. Note that at first, the code might not compile. You will have to fill in some blanks first, and then your app will run.
